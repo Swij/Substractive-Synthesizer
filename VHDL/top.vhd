0a5ebef8ab3d79d43f102be5fd90b41b7658bec3
akerlund <fredrik.akerlund.lh@gmail.com>
2016-02-21 23:22:52 +0100

    Testing ssh

--- VHDL/top.vhd
+++ VHDL/top.vhd
@@ -21,9 +21,6 @@
 
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
-
--- Uncomment the following library declaration if using
--- arithmetic functions with Signed or Unsigned values
 use IEEE.NUMERIC_STD.ALL;
 
 -- Uncomment the following library declaration if instantiating
