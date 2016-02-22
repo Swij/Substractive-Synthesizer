2c31c8031950e190e9947ba223c03ba39937a700
Dadeby <s.dadeby@gmail.com>
2016-02-22 16:57:54 +0100

    MIDI_to_Osc
    Added MIDI_to_Osc file as we talked about, also fixed the UART and added
    some to MIDI Decoder

---
+++ VHDL/MIDI_to_Osc.vhd
@@ -0,0 +1,47 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.NUMERIC_STD.all;
+
+ENTITY MIDI_to_Osc IS
+	PORT (
+		Data_in		: in STD_LOGIC_VECTOR(15 DOWNTO 0);
+		Note_on		: in STD_LOGIC;
+		Data_ready	: in STD_LOGIC;
+		Reset		: in STD_LOGIC;
+		
+		Note		: out STD_LOGIC_VECTOR(7 DOWNTO 0)
+	);
+END ENTITY;	
+	
+ARCHITECTURE MIDI_to_Osc_arch OF MIDI_to_Osc IS 
+
+	SIGNAL Note_int : INTEGER RANGE 0 to 127;
+	SIGNAL Velo_int : INTEGER RANGE 0 to 127;
+	
+BEGIN
+
+	PROCESS(Data_ready, Reset)
+	BEGIN
+		
+		IF (Reset = '1') THEN
+			Note <= (OTHERS => '0');
+			Note_int <= 0;
+			Velo_int <= 0;
+			
+		ELSIF (Data_ready='1') THEN
+			
+			Velo_int <= TO_INTEGER(SIGNED(Data_in(7 DOWNTO 0)));
+			
+			IF Velo_int = 0 OR Note_on = '0' THEN
+				Note <= (OTHERS => '0');
+				
+			ELSIF (Note_on='1') THEN
+				
+				Note_int <= TO_INTEGER(SIGNED(Data_in));
+				Note_int <= Note_int - 12;
+				Note <= STD_LOGIC_VECTOR(TO_UNSIGNED(Note_int,8));
+				
+			END IF;
+		END IF;
+	END PROCESS;
+END ARCHITECTURE;
