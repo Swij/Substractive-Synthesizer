11af45ba78c40618a3309b12ade9071a5da5ee43
Dadeby <s.dadeby@gmail.com>
2016-05-22 18:22:01 +0200

    Echo
    Looks fine in simulation

--- VHDL/Delay.vhd
+++ VHDL/Delay.vhd
@@ -6,13 +6,12 @@ use IEEE.NUMERIC_STD.ALL;
 
 ENTITY Delay IS
 		-- Delay time in ms
-	PORT(
+	PORT(	clk		: in STD_LOGIC;
+		reset	: in STD_LOGIC;
+		--clk_enable:in STD_LOGIC;
 		Sample	: in STD_LOGIC_VECTOR(11 DOWNTO 0);
-		Delay	: in INTEGER RANGE 0 to 2000;				-- Echo Delay in ms, <2s
-		Gain	: in INTEGER Range 0 to 7;				-- Gain of the Echo, 0/8 to 7/8
-		clk		: in STD_LOGIC;
-		Reset	: in STD_LOGIC;
-		
+		Delay_time	: in INTEGER RANGE 0 to 1999;				-- Echo Delay in ms, <2s
+		Gain	: in STD_LOGIC_VECTOR(11 DOWNTO 0);				-- Gain of the Echo, 0/8 to 7/8
 		Output	: Out STD_LOGIC_VECTOR(11 DOWNTO 0)
 	);
 END Delay;
@@ -24,32 +23,34 @@ ARCHITECTURE Delay_Arch OF Delay IS
 	SIGNAL Counter : INTEGER RANGE 0 to 79999:= 0;
 	SIGNAL DelayOffset : INTEGER Range 0 to 79999;
 	SIGNAL OutBuff : STD_LOGIC_VECTOR(11 DOWNTO 0);
-	SIGNAL OutBuffInt : INTEGER RANGE  -2048 to 2047;
-	SIGNAL Outvect : STD_LOGIC_VECTOR(14 DOWNTO 0);
+	SIGNAL OutBuffInt : STD_LOGIC_VECTOR(23 DOWNTO 0);
+	SIGNAL Outvect : STD_LOGIC_VECTOR(11 DOWNTO 0);
 	
 BEGIN
-	DelayOffset <= Delay*40;											  -- Number of steps back in the buffer for x ms delay
+
 Process(clk)
     BEGIN
-	IF (Reset = '1') THEN												  -- Standard Reset
+	IF (Reset = '0') THEN												  -- Standard Reset
 		CircBuffer <= (OTHERS=>(OTHERS=>'0'));
 		Counter <= 0;
 		
-	ELSIF RISING_EDGE(clk) THEN
-		CircBuffer(Counter) <= Sample;									  -- Save Data in to circBuffer
+	ELSIF (RISING_EDGE(clk)) THEN
+		CircBuffer(Counter) <= Sample;	                                  -- Save Data in to circBuffer
+		DelayOffset <= Delay_time*40;									  -- Number of steps back in the buffer for x ms delay								
 		
 		IF (DelayOffset > Counter) THEN									  -- Wrap around for counter
 			OutBuff(11 DOWNTO 0) <= CircBuffer(79999-(DelayOffset-Counter));
 		ELSE
 			OutBuff(11 DOWNTO 0) <= CircBuffer(Counter-DelayOffset);					  -- Load sound from previous Sample (Delay)
 		END IF;
-		OutBuffInt <= (To_integer(Signed(OutBuff)) * Gain);					  -- Multiply with gain
-		Outvect <= std_logic_vector(OutBuffInt);
-        	Output <= Outvect(14 DOWNTO 3);
+		OutBuffInt <= STD_LOGIC_VECTOR(SIGNED(OutBuff) * SIGNED(Gain));					  -- Multiply with gain
+		Outvect <= STD_LOGIC_VECTOR(SIGNED(Sample) + SIGNED(OutBuffInt(23 downto 12)));
+        --Output <= OutBuffInt(23 DOWNTO 12);
+		Output <= Outvect;
 		IF (Counter = 79999) THEN 
 			Counter <= 0;
 		ELSE
-		Counter <= Counter + 1;
+		    Counter <= Counter + 1;
 		END IF;
     END IF;
 END PROCESS;
