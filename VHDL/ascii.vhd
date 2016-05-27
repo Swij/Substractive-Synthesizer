35c0a0281b43126285c1b8615f1e6e180a966668
Simon Wijk <swij93@gmail.com>
2016-05-27 18:30:12 +0200

    Added zip with entire project

--- VHDL/ascii.vhd
+++ VHDL/ascii.vhd
@@ -16,7 +16,7 @@ package ascii is
     constant linebreak_to_BCD : std_logic_vector(7 downto 0) := std_logic_vector(to_unsigned(192+(16-BCD_row'LENGTH), 8)); -- 192 = 0xC0 which is new row
     constant types : mem_types := (
       (X"20",X"20",X"20",X"20",X"20",X"20",X"20",X"20",X"20",X"20",X"20",X"20",X"20",X"20",X"20",linebreak_to_BCD), --
-      (X"20",X"20",X"20",X"20",X"20",X"20",X"20",X"20",X"20",X"20",X"20",X"20",X"20",X"20",X"20",linebreak_to_BCD), -- 
+      (X"20",X"20",X"20",X"20",X"20",X"20",X"20",X"20",X"20",X"20",X"20",X"20",X"20",X"20",X"20",linebreak_to_BCD), --
       (X"4f",X"53",X"43",X"31",X"20",X"64",X"75",X"74",X"79",X"63",X"79",X"63",X"6c",X"65",X"3a",linebreak_to_BCD), -- OSC1 dutycycle:
       (X"4f",X"53",X"43",X"32",X"20",X"64",X"75",X"74",X"79",X"63",X"79",X"63",X"6c",X"65",X"3a",linebreak_to_BCD), -- OSC2 dutycycle:
       (X"4f",X"53",X"43",X"32",X"20",X"6f",X"66",X"66",X"73",X"65",X"74",X"3a",X"20",X"20",X"20",linebreak_to_BCD), -- OSC2 offset:
@@ -30,7 +30,7 @@ package ascii is
       (X"4c",X"46",X"4f",X"20",X"64",X"75",X"74",X"79",X"20",X"72",X"61",X"74",X"65",X"20",X"20",linebreak_to_BCD), -- LFO duty rate
       (X"4c",X"46",X"4f",X"20",X"6f",X"66",X"66",X"73",X"65",X"74",X"20",X"64",X"65",X"70",X"74",linebreak_to_BCD), -- LFO offset depth
       (X"4c",X"46",X"4f",X"20",X"6f",X"66",X"66",X"73",X"65",X"74",X"20",X"72",X"61",X"74",X"65",linebreak_to_BCD), -- LFO offset rate
-      (X"53",X"76",X"65",X"6E",X"3A",X"20",X"20",X"20",X"20",X"20",X"20",X"20",X"20",X"20",X"20",linebreak_to_BCD)); -- Sven
+      (X"20",X"20",X"20",X"20",X"20",X"20",X"20",X"20",X"20",X"20",X"20",X"20",X"20",X"20",X"20",linebreak_to_BCD));
 
 end ascii;
 
